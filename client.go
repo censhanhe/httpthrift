a23135f2a900a16664cd31273105a98c9130962b
David Taylor <tinystatemachine@gmail.com>
2015-09-28 23:51:38 +0000

    support getting url on-the-fly via callback

--- client.go
+++ client.go
@@ -9,7 +9,7 @@ import (
 
 type sendProt struct {
 	transport *http.Client
-	url       string
+	url       func() string
 	sendbuf   *thrift.TMemoryBuffer
 	recvbuf   *thrift.TMemoryBuffer
 
@@ -17,7 +17,7 @@ type sendProt struct {
 }
 
 func (t *sendProt) Flush() error {
-	req, err := http.NewRequest("POST", t.url, t.sendbuf)
+	req, err := http.NewRequest("POST", t.url(), t.sendbuf)
 	req.Header.Set("Content-Length", string(t.sendbuf.Len()))
 	req.Header.Set("Content-Type", "application/x-thrift")
 
@@ -31,16 +31,20 @@ func (t *sendProt) Flush() error {
 	return nil
 }
 
-func getSendProt(url string, recvbuf *thrift.TMemoryBuffer) thrift.TProtocol {
+func getSendProt(url func() string, recvbuf *thrift.TMemoryBuffer) thrift.TProtocol {
 	sendbuf := thrift.NewTMemoryBuffer()
 	underlying := thrift.NewTBinaryProtocol(sendbuf, true, true)
 	return &sendProt{&http.Client{}, url, sendbuf, recvbuf, underlying}
 }
 
-// pass these to the generated `NewFooClientProtocol(nil, recv, send)` method.
-func NewClientProts(url string) (recv, send thrift.TProtocol) {
+func NewDynamicClientProts(url func() string) (recv, send thrift.TProtocol) {
 	recvbuf := thrift.NewTMemoryBuffer()
 	send = getSendProt(url, recvbuf)
 	recv = thrift.NewTBinaryProtocol(recvbuf, true, true)
 	return recv, send
+}
+
+// pass these to the generated `NewFooClientProtocol(nil, recv, send)` method.
+func NewClientProts(url string) (recv, send thrift.TProtocol) {
+	return NewDynamicClientProts(func() string { return url })
 }
